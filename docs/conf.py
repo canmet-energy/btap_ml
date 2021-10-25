55d6dd56efb26a3c05f0a0f41bcb74e6ceb92b29
Bukola Ishola <maybuksho@yahoo.com>
2021-10-25 18:36:42 +0000

    Enable useful extensions for easier doc writing

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,11 @@ author = 'Natural Resources Canada, Statistics Canada'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

64eb7c064423e2a2ff9f07d055ae4a256a59d8c2
Reginald Maltais <reginald.maltais@statcan.gc.ca>
2021-11-02 15:17:34 +0000

    Use a pydantic settings class to centralize config vals

--- src/config.py
+++ src/config.py
@@ -1,10 +1,37 @@
 import json
+from pathlib import Path
+from typing import Any, Dict
 
 import pandas as pd
 import s3fs
+from pydantic import BaseSettings, HttpUrl, SecretStr
 
 
-def establish_s3_connection(endpoint_url: str, access_key: str, secret_key: str) -> s3fs.S3FileSystem:
+# There's a JSON file available with required credentials in it
+def json_config_settings_source(settings: BaseSettings) -> Dict[str, Any]:
+    return json.loads(settings.__config__.json_settings_path.read_text())
+
+class Settings(BaseSettings):
+    MINIO_URL: HttpUrl
+    MINIO_ACCESS_KEY: str
+    MINIO_SECRET_KEY: SecretStr
+
+    NAMESPACE: Path = Path('nrcan-btap')
+
+    class Config:
+        minio_tenant: str = 'standard'
+        json_settings_path: Path = Path(f'/vault/secrets/minio-{minio_tenant}-tenant-1.json')
+        # Ignore extra values present in the JSON data
+        extra = 'ignore'
+
+        @classmethod
+        def customise_sources(cls, init_settings, env_settings, file_secret_settings):
+            return (init_settings, json_config_settings_source, env_settings, file_secret_settings)
+
+settings = Settings()
+
+
+def establish_s3_connection(endpoint_url: str, access_key: str, secret_key: SecretStr) -> s3fs.S3FileSystem:
     """Used to create a connection to an S3 data store.
 
     Args:
@@ -18,7 +45,7 @@ def establish_s3_connection(endpoint_url: str, access_key: str, secret_key: str)
     s3 = s3fs.S3FileSystem(
         anon=False,
         key=access_key,
-        secret=secret_key,
+        secret=secret_key.get_secret_value(),
         client_kwargs={
             'endpoint_url': endpoint_url,
         }
@@ -40,25 +67,23 @@ def access_minio(tenant, bucket, path, operation, data):
     Returns:
        Dataframe containing the data downladed from minio is returned for read operation and for write operation , null value is returned.
     """
-    with open(f'/vault/secrets/minio-{tenant}-tenant-1.json') as f:
-        creds = json.load(f)
-
-    minio_url = creds['MINIO_URL']
-    access_key = creds['MINIO_ACCESS_KEY'],
-    secret_key = creds['MINIO_SECRET_KEY']
-
     # Establish S3 connection
-    s3 = establish_s3_connection(minio_url, access_key, secret_key)
+    s3 = establish_s3_connection(settings.MINIO_URL,
+                                 settings.MINIO_ACCESS_KEY,
+                                 settings.MINIO_SECRET_KEY)
+
+    # s3fs doesn't seem to like Path objects, so use a posix path string for operations
+    full_posix_path = settings.NAMESPACE.joinpath(path).as_posix()
 
     if operation == 'read':
         if 'csv' in path:
-            data = pd.read_csv(s3.open('{}/{}'.format(bucket, path), mode='rb'))
+            data = pd.read_csv(s3.open(full_posix_path, mode='rb'))
         elif 'xls' in path:
-            data = pd.read_excel(s3.open('{}/{}'.format(bucket, path), mode='rb'))
+            data = pd.read_excel(s3.open(full_posix_path, mode='rb'))
         else:
-            data = s3.open('{}/{}'.format(bucket, path), mode='rb')
+            data = s3.open(full_posix_path, mode='rb')
     else:
-        with s3.open('{}/{}'.format(bucket, path), 'wb') as f:
+        with s3.open(full_posix_path, 'wb') as f:
             f.write(data)
             data = ''
 

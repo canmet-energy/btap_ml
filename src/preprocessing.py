90628865e2556c1a2628ab10c821c000d3735ad0
Bukola Ishola <maybuksho@yahoo.com>
2021-11-19 20:14:33 +0000

    Document threshold reasoning in clean_data function

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -30,8 +30,12 @@ def clean_data(df):
     """
     Basic cleaning of the data using the following criterion:
     - dropping any column with more than 50% missing values
-    - dropping columns with less than 3 unquie values
-
+    The 50% threshold is a way to eliminate columns with too much missing values in the dataset. 
+   We cant use N/A as it will elimnate the entire row /datapoint_id. Giving the number of features we have to work it its better we eliminate 
+   columns with features that have too much missing values than to eliminate by rows, which is what N/A will do .
+    - dropping columns with 2 unquie values
+    For columns with 2 or 1 unique values are dropped during data cleaning as they have low variance 
+    and hence have little or no significant contribution to the accuracy of the model. 
     Args:
         df: dataset to be cleaned
 

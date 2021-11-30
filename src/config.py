b3dfcd86cdb71d2cbf19c180251582be915a16c9
Reginald Maltais <reginald.maltais@statcan.gc.ca>
2021-11-30 17:00:27 +0000

    Fix rst syntax errors

--- src/config.py
+++ src/config.py
@@ -52,10 +52,12 @@ class Settings(BaseSettings):
 
 def establish_s3_connection(endpoint_url: str, access_key: str, secret_key: SecretStr) -> s3fs.S3FileSystem:
     """Used to create a connection to an S3 data store.
+    
     Args:
         endpoint_url: The URL for the data store.
         access_key: The access key used to access the data store.
         secret_key: The secret key used to access the data store.
+        
     Returns:
         An s3fs file system object.
     """
@@ -78,11 +80,13 @@ def establish_s3_connection(endpoint_url: str, access_key: str, secret_key: Secr
 def access_minio(path: str, operation: str, data: Union[str, pd.DataFrame]):
     """
     Used to read and write to minio.
+    
     Args:
         tenant: default value is standard
         bucket: nrcan-btap
         path: file path where data is to be read from or written to
         data: for write operation, it contains the data to be written to minio
+        
     Returns:
        Dataframe containing the data downladed from minio is returned for read operation and for write operation , null value is returned.
     """

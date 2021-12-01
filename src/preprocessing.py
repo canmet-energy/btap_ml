f02972a67bc41b690e916e7c0ff929c050fe565c
Reginald Maltais <reginald.maltais@statcan.gc.ca>
2021-12-01 16:44:20 +0000

    Fix improper rst formatting on clean_data

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -35,13 +35,15 @@ logger = logging.getLogger(__name__)
 def clean_data(df) -> pd.DataFrame:
     """
     Basic cleaning of the data using the following criterion:
+
     - dropping any column with more than 50% missing values
-    The 50% threshold is a way to eliminate columns with too much missing values in the dataset.
-   We cant use N/A as it will elimnate the entire row /datapoint_id. Giving the number of features we have to work it its better we eliminate
-   columns with features that have too much missing values than to eliminate by rows, which is what N/A will do .
+      The 50% threshold is a way to eliminate columns with too much missing values in the dataset.
+      We cant use N/A as it will elimnate the entire row /datapoint_id. Giving the number of features we have to work it its better we eliminate
+      columns with features that have too much missing values than to eliminate by rows, which is what N/A will do .
     - dropping columns with 1 unique value
-    For columns with  1 unique values are dropped during data cleaning as they have low variance
-    and hence have little or no significant contribution to the accuracy of the model.
+      For columns with  1 unique values are dropped during data cleaning as they have low variance
+      and hence have little or no significant contribution to the accuracy of the model.
+
     Args:
         df: dataset to be cleaned
 
